ad8e3be20d6d7693e5a5e44501e8fde30c7d50e6
Tobias Conran Zorzetto <t166214@dac.unicamp.br>
2023-04-28 17:46:06 -0300

    Aula6

---
+++ Aula6/B.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+ 
+void solution(int n, int k){
+    
+    if( n%2 == 0 and k%2 == 1){
+        int a = n/k
+    }
+}
+ 
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        int k;
+        cin >> n >> k;
+        
+        solution(n, k);
+    }
+    return 0;
+}
